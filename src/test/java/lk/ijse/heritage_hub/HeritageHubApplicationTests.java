de5c6b93091260d459324d177a2488743cceb71b
Ishodhara <senanayakeabhimani@gmail.com>
2026-03-07 00:27:19 +0530

    1st commit- Created Springboot project

---
+++ src/test/java/lk/ijse/heritage_hub/HeritageHubApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.heritage_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeritageHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
